9429a3bf903c8852b72bf36242765224503acfbb
Alan Dominiczak <alan.dominiczak10@gmail.com>
2019-05-12 11:30:39 +0200

    Update weekly-weather styles

--- src/components/Weather/WeeklyWeather/WeeklyWeatherItem/WeeklyWeatherItem.js
+++ src/components/Weather/WeeklyWeather/WeeklyWeatherItem/WeeklyWeatherItem.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { getWeatherIcon } from './../../../../utils/getWeatherIcon';
 import { renderDayName } from '../../../../utils/renderDayName';
+import './WeeklyWeatherItem.scss';
 
 const WeeklyWeatherItem = props => {
 
     const iconID = props.icon;
+    const minTemp = Math.round(props.mintemp);
+    const maxTemp = Math.round(props.maxtemp);
 
     return (
         <div className="daily-weather">
             {renderDayName(props.date)}
-            <p className="daily-weather__temp-min">{props.mintemp}</p>
-            <p className="daily-weather__temp-max">{props.maxtemp}</p>
             <img src={getWeatherIcon(iconID)} className="daily-weather__icon" alt="Weather Icon" />
+            <div className="daily-weather__temp">
+                <p className="daily-weather__temp-value daily-weather__temp-value--max">{maxTemp} &#176;</p>
+                <p className="daily-weather__temp-value daily-weather__temp-value--min">{minTemp} &#176;</p>
+            </div>
         </div>
     );
 }
