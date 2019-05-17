6264abaf338a451c756d1ab242fd31dcfe3f826e
Alan Dominiczak <alan.dominiczak10@gmail.com>
2019-05-17 12:56:57 +0200

    Updates

---
+++ precache-manifest.cd56aa41633f988c59e24acd1a1fd56a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea86fde3ac912f80a1ce40010ec9aa12",
+    "url": "/your-weather/index.html"
+  },
+  {
+    "revision": "3c828f2353ac95f088f5",
+    "url": "/your-weather/static/css/main.ca05e03d.chunk.css"
+  },
+  {
+    "revision": "9866ca2f43601074d2c8",
+    "url": "/your-weather/static/js/2.a6048855.chunk.js"
+  },
+  {
+    "revision": "3c828f2353ac95f088f5",
+    "url": "/your-weather/static/js/main.6d9d04a8.chunk.js"
+  },
+  {
+    "revision": "d90b279174e1e88c55fe",
+    "url": "/your-weather/static/js/runtime~main.d215bba5.js"
+  },
+  {
+    "revision": "7671488d17ef27a713a10a70e280b5ae",
+    "url": "/your-weather/static/media/cloud.7671488d.png"
+  },
+  {
+    "revision": "7e666543ac7220d4f242573c63f18da2",
+    "url": "/your-weather/static/media/cloudiness.7e666543.svg"
+  },
+  {
+    "revision": "c073570774b99c9ec6b9257ac72c8ef1",
+    "url": "/your-weather/static/media/date.c0735707.svg"
+  },
+  {
+    "revision": "dda3001173487db3dcb00cd3f835378d",
+    "url": "/your-weather/static/media/humidity.dda30011.svg"
+  },
+  {
+    "revision": "2b7c48a767e22546941d70afc9f27ff0",
+    "url": "/your-weather/static/media/location.2b7c48a7.svg"
+  },
+  {
+    "revision": "1ea0bdf2dfbcebc1b72f2b66607cb345",
+    "url": "/your-weather/static/media/pressure.1ea0bdf2.svg"
+  },
+  {
+    "revision": "b3304a3df7b6e6e57d9ed67df0fab7e6",
+    "url": "/your-weather/static/media/wind.b3304a3d.svg"
+  }
+]);
